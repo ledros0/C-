94405cc9527133234ad5016000444bc3e75b2af3
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-02-16 18:41:37 -0300

    Add files via upload

---
+++ Calcular média por lista/Calcular média por lista/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+List<double> ListaDeNotas = new List<double>();
+
+double média = 0;
+Console.WriteLine("Digite as 5 notas de um aluno.");
+for (int i = 0; i < 5; i++)
+{
+    string notas = Console.ReadLine();
+    double notasDouble = double.Parse(notas);
+
+    ListaDeNotas.Add(notasDouble);
+}
+
+foreach (double notasDouble in ListaDeNotas)
+{
+    média += notasDouble; 
+    
+}
+média = média / 5;
+Console.WriteLine($"A média do aluno foi = {média:F1}");
