f0281f21ba229786146454ba41282ed4052c9ad0
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-02-17 22:33:09 -0300

    Add files via upload

---
+++ aula 17 02/aula 17 02/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_17_02
+{
+    internal class Carro
+    {
+        public string cor;
+        public int velocidade;
+
+        public void andar()
+        {
+            Console.WriteLine($"O carro {cor} andou");
+        }
+
+        public void acelerar(int aceleracao)
+        {
+            velocidade += aceleracao;
+        }
+    }
+}
---
+++ aula 17 02/aula 17 02/Program.cs
@@ -0,0 +1,11 @@
+﻿using aula_17_02;
+
+Carro PrimeiroCarro = new Carro();
+PrimeiroCarro.cor = "índigo";
+
+Carro SegundoCarro = new Carro();
+SegundoCarro.cor = "branco";
+
+//Console.Write($"{PrimeiroCarro.cor} ");
+
+PrimeiroCarro.andar();
