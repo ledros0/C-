24a49174fb3c4461221a4877330ec40cccf0b1bb
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-03-29 16:29:57 -0300

    Add files via upload

---
+++ Cálculo de área de Triângulo, Quadrado e  Círculo/Cálculo de área de Triângulo, Quadrado e  Círculo/Calculo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_de_área_de_Triângulo__Quadrado_e__Círculo
+{
+    internal class Calculo
+    {
+        private double Base;
+        private double Altura;
+        private double Lado;
+        private double Raio;
+        private static double PI = 3.14;
+        private double Area;
+
+        public class Triangulo : Calculo
+        {
+            public Triangulo(double Base, double Altura)
+            {
+                this.Base = Base;
+                this.Altura = Altura;
+
+                Area = (Base * Altura)/2;
+
+                Console.WriteLine($"A área do triângulo é {Area:N2}\n");
+            }
+        }
+        public class Quadrado : Calculo 
+        {
+            public Quadrado(double Lado)
+            {
+                this.Lado = Lado;
+
+                Area = Lado * Lado;
+
+                Console.WriteLine($"A área do quadrado é {Area:N2}\n");
+            }
+        }
+        public class Circulo : Calculo
+        {
+            public Circulo(double Raio)
+            {
+                this.Raio = Raio;
+
+                Area = PI * (Raio * Raio);
+
+                Console.WriteLine($"A área do círculo é {Area:N2}\n");
+            }
+        }
+    }
+}
---
+++ Cálculo de área de Triângulo, Quadrado e  Círculo/Cálculo de área de Triângulo, Quadrado e  Círculo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Cálculo_de_área_de_Triângulo__Quadrado_e__Círculo;
+
+Console.WriteLine("Digite as especificações do triângulo. (Base e Altura)");
+double BaseT = Convert.ToDouble(Console.ReadLine());
+double AlturaT = Convert.ToDouble(Console.ReadLine());
+var tri = new Calculo.Triangulo(BaseT, AlturaT);
+
+Console.WriteLine("Digite as especificações do quadrado. (Tamanho do lado)");
+double Lado = Convert.ToDouble(Console.ReadLine());
+var qua = new Calculo.Quadrado(Lado);
+
+Console.WriteLine("Digite as especificações do quadrado. (Raio)");
+double Raio = Convert.ToDouble(Console.ReadLine());
+var cir = new Calculo.Circulo(Raio);
