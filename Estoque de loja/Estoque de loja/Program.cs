14f15f02ce84a4b4d3bac1aab89a71139365b083
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-05-19 00:10:35 -0300

    Delete Estoque de loja directory

--- Estoque de loja/Estoque de loja/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Estoque_de_loja;
-
-Alimento CocaCola = new Alimento("Coca-cola", "29/09/2029", 2.50, "Latinha de coca");
-Roupa Blusa = new Roupa("Blusa", "G", 56.50, "Blusa azul quentinha");
-Eletronico Celular = new Eletronico("Sansung", "S22 Ultra", 3.500, "Celular bombastico");
-Loja1 Loja_do_ze = new Loja1();
-
-Loja_do_ze.EstocarProduto(CocaCola, 6);
